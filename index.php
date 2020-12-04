0d5dc737d2b6299be1b4d11a65f6828b1b2cb341
OnyisoChris <onyisochris1@gmail.com>
2020-12-04 09:36:58 +0300

    My Test Site

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+
+  <head>
+      <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Golden Trash Investment</title>
+	  <link rel="stylesheet" href="style.css">
+	  <link href="https://fonts.googleleapis.com/css2?family
+	  =Poppins:wght@300;4005;500;600;700&display=swap"
+	  rel="stylesheet">
+	  </head>
+		   <body>
+		   	<section id="nav-bar"><nav class="navbar navbar-expand-lg navbar-light">
+<a class="navbar-brand" href="#"></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+<ul class="navbar-nav">
+<li class="nav-item active">
+<a class="nav-link" href="#">HOME<span class="sr-only">
+</span></a>
+</li>
+<br>
+<li class="nav-item">
+<a class="nav-link" href="#">ABOUT US</a>
+</li><br>
+
+<li class="nav-item">
+<a class="nav-link" href="#">PRODUCTS</a>
+</li><br>
+
+<li class="nav-item">
+<a class="nav-link" href="#">THE TEAM</a>
+</li><br>
+
+<li class="nav-item">
+<a class="nav-link" href="#">CONTACT US</a>
+</li>
+</ul>
+</div>
+</nav>
+
+</section>
+<!-------Slider----->
+<div id="slider">
+	<div id="headerSlider" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#headerSlider" data-slide-to="0" class="active"></li>
+    <li data-target="#headerSlider" data-slide-to="1"></li>
+    <li data-target="#headerSlider" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="img/banner.png">
+      <div class="carousel-caption"></div>
+      	<h1>Dumpsite</h1>
+</div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="img/banner1.png">
+      <div class="carousel-caption"></div>
+      	<h2>A collection of plastic waste</h2>
+    
+</div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="img/banner3.png">
+      <div class="carousel-caption"></div>
+      	<h3>Poorly dumped plastic waste</h3>
+      </div>
+    
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="img/banner4.png">
+      <div class="carousel-caption"></div>
+      	<h3>Poorly dumped plastic waste</h3>
+    </div>
+  <a class="carousel-control-prev" href="#headerSlider" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#headerSlider" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+	<div id="headerSlider">
+	</div>
+		</div>
+		<!-----About----->
+		<section id="about">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<h2>About Us</h2>
+						<div class="about-content">
+							Golden Trash Investment is an enterprise that manufacture house ventilators from plastic waste with the aim of conserving the environment and creating jobs. We also make products such as pavement blocks all from plastic waste. We are a team of motivated individuals with passion for developing our community.
+						</div>
+						<button type="button" class="btn btn-primary">Read more>>
+						</button>
+					</div>
+
+
+					<div class="col-md-6">
+					<p>House Ventilators</p>
+					<div class="progress">
+						<div class="progress-bar" style="width: 80%;">80%</div>
+						<div class="col-md-6">
+					<p>Partitioning blocks</p>
+					<div class="progress">
+						<div class="progress-bar" style="width: 80%;">80%</div>
+						<p>Pavement block</p>
+					<div class="progress">
+						<div class="progress-bar" style="width: 80%;">80%</div>
+						
+					</div>
+					</div>
+				
+				<!-------products------>
+			<
+                 <section id="Products">
+ 	             <div class="container">
+ 		          <h1>Products</h1>
+ 		           <div class="row">
+ 		     	  <div class="col-md-3 profile-pic text-center">
+ 		     		<div class="img-box">
+ 		     			<img src="img/vent.png"class="img-responsive">
+ 		     				<h2>House Ventilator</h2>
+ 		     				<img src="img/snip 1.png"class="img-responsive">
+ 		     				<h2>House Ventilator</h2>
+ 		     				<p>Get house ventilators at affordable price. A pair @ ksh100 and 1 piece @  ksh 50  </p>
+				<img src="img/part.png">			
+				<h2>Pavement block</h2>
+				<p>Get house ventilators at affordable price. A pair @ ksh100 and 1 piece @  ksh 50  </p>
+
+				<img src="img/part2.png">			
+				
+				<h2>Partitioning Block</h2>
+				<p>Get house partioning block at affordable price.</p>
+			</div>
+		</div>
+	</div>
+	</div>
+    </section>
+    <!------Team members------>
+    <section id="team">
+ 	<div class="container">
+ 		     <h1>Our Team</h1>
+ 		     <div class="row">
+ 		     	<div class="col-md-3 profile-pic text-center">
+ 		     		<div class="img-box">
+ 		     			<img src="img/team1.jpg"class="img-responsive">
+ 		     			<ul>
+ 		     				<a href="#"><li><i class="fa fa-facebook"></i></li></a>
+ 		     				<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+ 		     				<a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+ 		     				<h2>Robert Karuru</h2>
+ 		     				<h3>The Founder</h3>
+ 		     			</ul>
+ 		     			<img src="img/team2.jpg"class="img-responsive">
+ 		     			<ul>
+ 		     				<a href="#"><li><i class="fa fa-facebook"></i></li></a>
+ 		     				<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+ 		     				<a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+ 		     				<h2>Elizabeth Atieno</h2>
+ 		     				<h3>Co-founder</h3>
+ 		     			</ul>
+ 		     				</div>
+ 		     				</div>
+ 		     				</div>
+ 		     			
+
+
+
+ 		 </div>
+ 		</section>
+ 		<!-------Contact Us------>
+ 			<section id="contact">
+ 				<div class="container">
+ 					<h1>Contact Us</h1>
+ 					<div class="row">
+ 						<div class="col-md-6">
+ 							<form class="contact-form">
+ 								<div class="form-group">
+ 									<input type="text" class="form-control" placeholder="Your name">
+ 								</div>
+ 								<div class="form-group">
+ 									<input type="number" class="form-control" placeholder="Phone no">
+ 								</div>
+ 								<div class="form-group">
+ 									<input type="email" class="form-control" placeholder="Email Id">
+ 								</div>
+ 								<div class="form-group">
+ 									<textarea class="form-control" placeholder="Message"></textarea>
+ 								</div>
+ 								<button type="submit" class="btn btn-primary">SEND MESSAGE</button>
+ 							</form>
+
+ 						</div>
+ 						<div class="col-md-6 contact-info">
+ 							<div class="follow"><b>Address:</b>Nyeri town</div>
+ 							</div>
+ 						</div>
+ 					</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
